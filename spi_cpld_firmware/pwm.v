16d1807aa65a1d1b592fdbbb64a3fc0e502b9b40
fedor <fedor@yu.wildpark.net>
2019-01-12 09:41:56 +0200

    add

---
+++ spi_cpld_firmware/pwm.v
@@ -0,0 +1,19 @@
+module pwm(in,clk,out);
+input[7:0] in;
+input clk;
+
+output out;
+
+reg[7:0] counter;
+reg out;
+
+always@(posedge clk)
+begin
+if(counter<=in) 
+    out <=1'b1;
+else 
+    out <=1'b0;
+counter<=counter+8'b1;
+end
+
+endmodule
