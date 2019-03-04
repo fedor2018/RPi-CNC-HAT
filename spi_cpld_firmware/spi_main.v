a544b45b4cecaff130fde9a49a04456f8bc1afef
fedor@yu.wildpark.net <fedor@yu.wildpark.net>
2019-03-04 14:43:05 +0200

    update

--- spi_cpld_firmware/spi_main.v
+++ spi_cpld_firmware/spi_main.v
@@ -16,7 +16,7 @@
 //    along with this program; if not, write to the Free Software
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //**********************************************************************
-`define USE_SERVO
+//`define USE_SERVO
 // Open-Drain buffer
 //`define OD
 `ifdef OD
@@ -48,9 +48,9 @@ input rin;//rpm in
 reg Spolarity;
 
 reg[O-1:0] real_dout; output [O-1:0] dout = /*do_tristate ? {O-1{1'bZ}} :*/ real_dout; 
-wire[3:0] real_step; output [3:0] step = /*do_tristate ? 4'bZ :*/ real_step ^ {4{Spolarity}};
-wire[3:0] real_dir; output [3:0] dir = /*do_tristate ? 4'bZ :*/ real_dir;
-wire real_pout; output pout = /*do_tristate ? 1'bZ :*/ (INV_PWM)?~real_pout:real_pout;
+wire[3:0] real_step; output [3:0] step = do_tristate ? 4'b0 : real_step ^ {4{Spolarity}};
+wire[3:0] real_dir; output [3:0] dir = do_tristate ? 4'b0 : real_dir;
+wire real_pout; output pout = do_tristate ? 1'b0 : (INV_PWM)?~real_pout:real_pout;
 
 `ifdef OD
 OC_Buff ocout[O-1:0](real_dout, dout);
