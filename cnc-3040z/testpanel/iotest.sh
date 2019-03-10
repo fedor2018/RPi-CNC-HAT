daa660eaeddafaf1f4d49b8e824aa1347717d665
fedor2018 <fedor@yu.wildpark.net>
2019-03-10 17:24:08 +0200

    update

---
+++ cnc-3040z/testpanel/iotest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#update the next one to fit your path, 
+#it does work for me executed from this config dir
+export DEBUG=5
+halrun -U
+realtime start
+
+pyvcp -c pyvcp iotest.xml &
+
+#increase the sleep if you have a very slow machine
+sleep 1
+
+halcmd -f < ./testpanel.hal
+#sleep 3
+#halrun -U
+
+
